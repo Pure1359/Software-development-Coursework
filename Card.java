f999dd9a50aa52aee1e203988e91d9a56d0cecde
BradleyR06 <jbr207@exeter.ac.uk>
2025-10-16 15:46:10 +0000

    create card class

---
+++ Card.java
@@ -0,0 +1,15 @@
+public class Card {
+    private final int value; // The face value of the card
+
+    // Constructor for Card
+    public Card(int value) {
+        this.value = value;
+    }
+
+    // Getter method for Card
+    public int getValue() {
+        return value;
+    }
+
+    // No setter method required because card value is fixed
+}
